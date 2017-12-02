a29c592517fb7510b1577de057a213044f456396
Sumit Paroothi <paroothisumit@gmail.com>
2017-12-03 03:49:09 +0530

    Integration of gui with backend

---
+++ clientB/bridge.py
@@ -0,0 +1,15 @@
+import requests
+
+
+# original
+def get_client_info(id,server_address):
+    response = requests.post(server_address + 'get_site_info', json={'id': id})
+    return response.json()
+
+# dummy
+# def get_client_info(id,server_address):
+#     ret = dict()
+#     ret["description"] = "it is a description"
+#     ret["contact"] = "saar2119@gmail.com"
+#     ret["address"] = "Room no 55, Tilak, MNNIT"
+#     return ret
